409f59f06b61d1d47c06fa881609231c062e6429
vjchevakula@gmail.com <vjchevakula@gmail.com>
2021-09-12 16:01:28 -0700

    b/4: updated print statement in disk_usage

--- disk_usage.py
+++ disk_usage.py
@@ -9,7 +9,7 @@ stat = shutil.disk_usage(path)
 gb = 10 ** 9
   
 # Print disk usage statistics
-print("Disk usage statistics:")
+print("Disk usage statistics in your linux server:")
 print('Total Space: {:6.2f} GB'.format(stat[0] /gb))
 print('Total Used Space: {:6.2f} GB'.format(stat[1] /gb))
 print('Total Free Space: {:6.2f} GB'.format(stat[2] /gb))
