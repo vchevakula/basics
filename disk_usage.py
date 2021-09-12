827110baa2e562add48c47ff1267e4819e5a5c19
vjchevakula@gmail.com <vjchevakula@gmail.com>
2021-09-12 15:27:26 -0700

    b/3: Updated disk usage specific details

--- disk_usage.py
+++ disk_usage.py
@@ -6,7 +6,10 @@ path = "/home/vchevakula/"
 # Get the disk usage statistics
 # about the given path
 stat = shutil.disk_usage(path)
+gb = 10 ** 9
   
 # Print disk usage statistics
 print("Disk usage statistics:")
-print(stat)
+print('Total Space: {:6.2f} GB'.format(stat[0] /gb))
+print('Total Used Space: {:6.2f} GB'.format(stat[1] /gb))
+print('Total Free Space: {:6.2f} GB'.format(stat[2] /gb))
